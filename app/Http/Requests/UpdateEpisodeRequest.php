369e70fa3798cb8e2077ad84fce307cd5472bf74
David Igbo <davidokpah396@gmail.com>
2025-04-22 12:37:15 +0100

    modify files

---
+++ app/Http/Requests/UpdateEpisodeRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateEpisodeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'podcast_id' => 'required|exists:podcasts,id',
+            'title' => 'required|string|max:255',
+            'audio_url' => 'required|url',
+            'duration' => 'required|string|max:255',
+            'release_date' => 'required|date',
+        ];
+    }
+}
--- database/migrations/2025_04_22_110859_create_podcasts_table.php
+++ database/migrations/2025_04_22_110859_create_podcasts_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('podcasts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('category_id')->constrained()->onDelete('cascade');
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('title');
             $table->string('description');
             $table->string('image');
